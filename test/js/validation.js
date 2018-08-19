8dcbe0e6af966fb8a673acb280e46be80466d627
yehorDorosh <oggy300586@gmail.com>
2018-08-19 14:19:16 +0300

    form JS

--- test/js/validation.js
+++ test/js/validation.js
@@ -1,180 +1,116 @@
 'use strict'
-//alert('Hellow world');
-let nameTooltip = null;
-let surNameTooltip = null;
-let telTooltip = null;
-let emailTooltip = null;
+//--------------------------------------- message block
+let messageBlock;
 
-/*document.onmouseover =*/
-function showTooltip(e, l, t) {
-    let target = e; //.target;
+function showMessage() {
+    let elem = document.createElement('div');
+    elem.className = 'message'
+    document.body.appendChild(elem);
+    elem.style.left = 50 + '%';
+    elem.style.top = 50 + '%';
 
-    let tooltip = target.getAttribute('data-tooltip');
-    if (!tooltip) return;
+    let elemOK = document.createElement('div');
+    elemOK.className = 'ok-btn';
+    elemOK.innerHTML = 'OK';
+    document.body.lastChild.appendChild(elemOK);
+    elemOK.style.left = 50 + '%';
+    elemOK.style.top = 50 + '%';
 
-    let tooltipElem = document.createElement('div');
-    tooltipElem.className = 'tooltip';
-    tooltipElem.innerHTML = tooltip;
-    document.body.appendChild(tooltipElem);
-
-    let coords = target.getBoundingClientRect();
-
-    let left = l + coords.left + (target.offsetWidth - tooltipElem.offsetWidth) / 2;
-    if (left < 0) left = 0; // не вылезать за левую границу окна
-
-    let top = t + coords.top - tooltipElem.offsetHeight - 5;
-    if (top < 0) { // не вылезать за верхнюю границу окна
-        top = coords.top + target.offsetHeight + 5;
-    }
-
-    tooltipElem.style.left = left + 'px';
-    tooltipElem.style.top = top + 'px';
-
-    let showingTooltip = tooltipElem;
-    return showingTooltip;
-};
-
-/*document.onmouseout =*/
-function hideTooltip(tooltip) {
-
-    if (tooltip) {
-        document.body.removeChild(tooltip);
-        tooltip = null;
-        return tooltip;
-    }
-
-};
-
-//--------------------------------------
-
-function inputOnlyLetters(e) {
-    e = e || event;
-    if (e.ctrlKey || e.altKey || e.metaKey) return;
-    let chr = getChar(e);
-    if (chr == null) return;
-    if (chr >= '0' && chr <= '9') {
-        showError(e.target);
-        if (e.target.name == 'name') nameTooltip = showTooltip(e.target, 0, 30);
-        if (e.target.name == 'surname') surNameTooltip = showTooltip(e.target, 0, 30);
-        return false;
-    }
-    resetError(e.target);
-    if (e.target.name == 'name') nameTooltip = hideTooltip(nameTooltip);
-    if (e.target.name == 'surname') surNameTooltip = hideTooltip(surNameTooltip);
-
+    messageBlock = elem;
+    return true;
 }
 
-function inputTel(e) {
-    e = e || event;
-    if (e.ctrlKey || e.altKey || e.metaKey) return;
-    let chr = getChar(e);
-    if (chr == null) return;
-    if (!((chr >= '0' && chr <= '9') || chr == '+' || chr == '-' || chr == '(' || chr == ')')) {
-        showError(e.target);
-        if (e.target.name == 'tel') telTooltip = showTooltip(e.target, 0, 30);
-        return false;
-    }
-    resetError(e.target);
-    if (e.target.name == 'tel') telTooltip = hideTooltip(telTooltip);
-
-}
-
-function getChar(event) {
-    if (event.which == null) {
-        if (event.keyCode < 32) return null;
-        return String.fromCharCode(event.keyCode) // IE
-    }
-    if (event.which != 0 && event.charCode != 0) {
-        if (event.which < 32) return null;
-        return String.fromCharCode(event.which) // остальные
-    }
-    return null; // специальная клавиша
-}
-
-function checkMailMask(e) {
-    let target = e.target;
-    let arr = target.value.split('');
-    if (!arr.includes('@')) {
-        showError(target);
-        emailTooltip = showTooltip(target, 0, 30);
-        return false;
-    } else {
-        resetError(target);
-        emailTooltip = hideTooltip(emailTooltip);
-        return true;
+function hideMessage(messageBlock) {
+    if (messageBlock) {
+        document.body.removeChild(messageBlock);
+        messageBlock = null;
     }
 }
-
-//-----------------------------------------
+//----------------------------------------- validation
 
 function showError(inputElem) {
-    inputElem.classList.add('error');
+    if (typeof (inputElem) == 'object') inputElem.classList.add('error');
 }
 
 function resetError(inputElem) {
-    inputElem.classList.remove('error');
+    if (typeof (inputElem) == 'object') inputElem.classList.remove('error');
 }
+
+function checkValidInput(inputElem, mask, required) {
+    if (inputElem.value.match(mask) != null || (!required && !inputElem.value)) {
+        inputElem.isValid = true;
+    } else {
+        inputElem.isValid = false;
+        showError(inputElem);
+    }
+}
+
 
 function validate(form) {
     let elems = form.elements;
 
-    resetError(elems.name);
-    nameTooltip = hideTooltip(nameTooltip);
-    resetError(elems.surname);
-    surNameTooltip = hideTooltip(surNameTooltip);
-    resetError(elems.tel);
-    telTooltip = hideTooltip(telTooltip);
-    resetError(elems.email);
-    emailTooltip = hideTooltip(emailTooltip);
-
-
-    if (!elems.name.value) {
-        showError(elems.name);
-        nameTooltip = showTooltip(elems.name, 0, 30);
-    }
-    if (!elems.surname.value) {
-        showError(elems.surname);
-        surNameTooltip = showTooltip(elems.surname, 0, 30);
-    }
-    if (!elems.tel.value) {
-        showError(elems.tel);
-        telTooltip = showTooltip(elems.tel, 0, 30);
-    }
-    if (!elems.email.value) {
-        showError(elems.email);
-        emailTooltip = showTooltip(elems.email, 0, 30);
+    for (let key in elems) {
+        resetError(elems[key]);
     }
 
+    checkValidInput(elems.name, /^[а-яА-ЯёЁa-zA-Z]+$/, true);
+    checkValidInput(elems.surname, /^[а-яА-ЯёЁa-zA-Z]+$/, true);
+    checkValidInput(elems.tel, /^[0-9()\-+ ]+$/, true);
+    checkValidInput(elems.email, /[a-zA-Z0-9_]+@[a-zA-Z0-9_]/, false);
+    // /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/
 
-    if (elems.name.value && elems.surname.value && elems.tel.value && elems.email.value && checkMailMask(email.value)) return true;
-
+    for (let key in elems) {
+        if (typeof (elems[key]) == 'object') {
+            if (elems[key].isValid == false) return false;
+        }
+    }
+    return false;
 }
-//-------------------------------------
+//-------------------------------------- promise
+
+function request() {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            resolve(showMessage());
+        }, 2000);
+    });
+}
+
+//------------------------------------- common
 let form = document.forms.registration;
 let btnSubmit = document.getElementsByName('submit');
 
 
-//---------------------------------------
+//--------------------------------------- events
 
 form.onclick = function (e) {
     let target = e.target;
 
     if (target.name != 'submit') {
         resetError(target);
-        if (target.name == 'name') nameTooltip = hideTooltip(nameTooltip);
-        if (target.name == 'surname') surNameTooltip = hideTooltip(surNameTooltip);
-        if (target.name == 'tel') telTooltip = hideTooltip(telTooltip);
-        if (target.name == 'email') emailTooltip = hideTooltip(emailTooltip);
-        form.elements.name.onkeypress = inputOnlyLetters;
-        form.elements.surname.onkeypress = inputOnlyLetters;
-        form.elements.tel.onkeypress = inputTel;
-        form.elements.email.onblur = checkMailMask;
     }
 
     if (target.name == 'submit') {
         form.onsubmit = function () {
             if (!validate(form)) return false;
+            request()
+                .then(
+                    response => {
+                        btnSubmit[0].disabled = true;
+                        btnSubmit[0].classList.remove('btn');
+                        btnSubmit[0].classList.add('btn-disabled');
+                    },
+                    error => {}
+                );
+            return false;
         }
     }
+}
 
+document.body.onclick = function (e) {
+    let target = e.target;
+
+    if (target.classList.contains('ok-btn')) {
+        hideMessage(messageBlock);
+    }
 }
