c8a3fd693156562172e5186fb222d1c178910d89
yehorDorosh <oggy300586@gmail.com>
2018-08-19 19:19:57 +0300

    bug table

---
+++ test/js/table.js
@@ -0,0 +1,23 @@
+//---------------format values in table
+let deposit = [];
+let account = [];
+let newDeposit = [];
+let newAccount = [];
+
+for (let i = 1; i < table.rows.length; i++) {
+    deposit[i - 1] = table.rows[i].cells[1].innerText;
+    account[i - 1] = table.rows[i].cells[2].innerText;
+}
+
+for (let i = 0; i < deposit.length; i++) {
+    let num = deposit[i].slice(1);
+    let formatNum = new Intl.NumberFormat('ru-RU', {}).format(num);
+    newDeposit.push('$' + formatNum);
+
+    newAccount.push(new Intl.NumberFormat('ru-RU', {}).format(account[i]));
+}
+
+for (let i = 1; i < 11; i++) {
+    table.rows[i].cells[1].innerText = newDeposit[i - 1];
+    table.rows[i].cells[2].innerText = newAccount[i - 1];
+}
